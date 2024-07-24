c940b8dbad6f65ae0a7dcaad334ecc442cf7f782
Mohammed Rayaan Pasha <134199206+mdrayaanpasha@users.noreply.github.com>
2024-07-24 22:27:57 +0530

    Create charat.java

---
+++ charat.java
@@ -0,0 +1,15 @@
+//Strings - charat
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        String str = "MEHRAN";
+      
+        
+        
+
+        System.out.println(str.charAt(1));  
+    
+    }
+}
