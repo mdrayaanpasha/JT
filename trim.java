13f75c6af3a13d7d17596369a8d0d256e18c4a22
Mohammed Rayaan Pasha <134199206+mdrayaanpasha@users.noreply.github.com>
2024-07-24 22:26:50 +0530

    Create trim.java

---
+++ trim.java
@@ -0,0 +1,12 @@
+//Strings - Trim
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        String str = "MEHRAN      ";
+
+        System.out.println(str.trim()); //removes blankspace
+    
+    }
+}
