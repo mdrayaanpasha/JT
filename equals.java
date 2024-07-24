f61c48c790a4d946cd83bea69489d53ae8608547
Mohammed Rayaan Pasha <134199206+mdrayaanpasha@users.noreply.github.com>
2024-07-24 22:27:20 +0530

    Create equals.java

---
+++ equals.java
@@ -0,0 +1,15 @@
+//Strings - equals
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        String str = "MEHRAN";
+        String str2 = "Mehran";
+        
+        
+
+        System.out.println(str.equals(str2)); 
+    
+    }
+}
