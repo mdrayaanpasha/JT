08c99dd719f29e6df621a67f39957a1bfd1d298d
Mohammed Rayaan Pasha <134199206+mdrayaanpasha@users.noreply.github.com>
2024-07-24 22:28:34 +0530

    Create concat.java

---
+++ concat.java
@@ -0,0 +1,12 @@
+//Strings - Concat
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        String str = "MEHRAN";
+        String str2 = " AHMED";
+        System.out.println(str.concat(str2));  
+    
+    }
+}
