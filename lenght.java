0ed63558f9a4ec236d1d836266025ec09cdf5dfe
Mohammed Rayaan Pasha <134199206+mdrayaanpasha@users.noreply.github.com>
2024-07-24 22:27:36 +0530

    Create lenght.java

---
+++ lenght.java
@@ -0,0 +1,15 @@
+//Strings - length
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        String str = "MEHRAN";
+      
+        
+        
+
+        System.out.println(str.length());  //lenght of string
+    
+    }
+}
