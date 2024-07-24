5667615cd1396f0412716c3dca1316f3ec1ff723
Mohammed Rayaan Pasha <134199206+mdrayaanpasha@users.noreply.github.com>
2024-07-24 22:26:09 +0530

    Create upperlower.java

---
+++ upperlower.java
@@ -0,0 +1,14 @@
+//upper and lowercase strings
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        String str = "MEHRAN";
+        
+        System.out.println(str.toLowerCase().toUpperCase());
+        
+        
+        
+    }
+}
