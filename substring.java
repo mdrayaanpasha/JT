782c6f91b3bc69534f39eee72e60b3c2833efcec
Mohammed Rayaan Pasha <134199206+mdrayaanpasha@users.noreply.github.com>
2024-07-24 22:28:15 +0530

    Create substring.java

---
+++ substring.java
@@ -0,0 +1,12 @@
+//Strings - substring
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        String str = "MEHRAN";
+      
+        System.out.println(str.substring(3));  
+    
+    }
+}
