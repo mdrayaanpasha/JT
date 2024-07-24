a94ad3b922fc6d32d49459d9ad771d1b76dbaefd
Mohammed Rayaan Pasha <134199206+mdrayaanpasha@users.noreply.github.com>
2024-07-24 22:26:30 +0530

    Create replace.java

---
+++ replace.java
@@ -0,0 +1,12 @@
+//Strings - Replace
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        String str = "MEHRAN";
+
+        System.out.println(str.replace("ME","R"));
+    
+    }
+}
